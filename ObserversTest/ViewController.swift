9afdbd21a92ce41523e414a7f1d60eea04924ff1
David Galán <david.galan@studiogenesis.es>
2020-11-28 17:31:02 +0100

    Initial Commit

---
+++ ObserversTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObserversTest
+//
+//  Created by David SG on 28/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
